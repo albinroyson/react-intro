af14af22622a54ce46dc6fc77354b66d00782351
Albin Royson <albinroyson@pop-os.localdomain>
2021-12-20 15:45:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from "react";
 class App extends React.Component {
   counter=10;
   render(){
-    return <h1>count is ={this.counter}</h1>
+    return (
+      <ul>
+        <li>apple</li>
+        <li>orange</li>
+        <li>Grapes</li>
+      </ul>
+    );
   }
 }
 
